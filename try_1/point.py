d3d18e10534a21fd687b372ce296592a4b1b3629
Vipea <fringsmax@gmail.com>
2019-04-11 15:05:12 +0200

    added class point and started in main

--- try_1/point.py
+++ try_1/point.py
@@ -8,3 +8,11 @@ class Point(Object):
 
     def get_neighbours(self):
         return self.neighbours
+
+    def get_attribute(self):
+        return self.atrribute
+
+    def __str__(self):
+        return(f"This point is at location {self.location} and is " +
+               f"{self.attribute} with {self.neighbours} as neighbours " +
+               f"and has value {self.value}")

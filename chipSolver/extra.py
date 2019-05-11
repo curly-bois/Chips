6dde8a8504a02f3918d67782bfed942a2ba16fe0
FlorisFok <43857896+FlorisFok@users.noreply.github.com>
2019-05-11 17:29:11 +0200

    Vizualsss

---
+++ chipSolver/extra.py
@@ -0,0 +1,130 @@
+from point import Point
+from wire import Wire
+
+import random
+import numpy as np
+import pandas as pd
+import time
+
+def get_wires(mainGrid, points_to_connect):
+    # Start the loop for all the wires
+    wires = []
+    connected = 0
+    wire_num = -1
+    not_connected = []
+
+    for start,end in points_to_connect:
+        wire_num += 1
+
+        # find line, else return empty dict
+        parent, tries = mainGrid.Astar(start, end)
+        if parent == {}:
+            not_connected.append((start, end))
+            pass
+        else:
+            # Retrace the line and laydown the wire
+            wire = mainGrid.make_wire(start, end, parent)
+            con_wire = Wire(wire_num, start, end, wire, tries)
+            wires.append(con_wire)
+
+            # Update value grid
+            mainGrid.update_layer()
+            # mainGrid.wire_NN_edit() ## Commented out
+            connected += 1
+
+    return wires, connected, not_connected
+
+def swap_wires(wires, not_connected, mainGrid):
+
+    old_wires = [i for i in wires]
+
+    for wire in old_wires:
+        not_con_len = len(not_connected)
+
+        start, end, number = mainGrid.remove_wire(wire)
+        wires2, connected2, not_connected2 = get_wires(mainGrid, not_connected)
+
+        if not_con_len - 1 >= len(not_connected2):
+            not_connected.append((start, end))
+            wires += wires2
+            not_connected = not_connected2
+        else:
+            mainGrid.add_wire(wire)
+
+    random.shuffle(not_connected)
+    return mainGrid, wires, not_connected
+
+def threedimdistance( i, j):
+    '''
+    Get three dimesional distance
+    '''
+    deltaxsquared = (i[0] - j[0]) ** 2
+    deltaysquared = (i[1] - j[1]) ** 2
+    deltazsquared = (i[2] - j[2]) ** 2
+    return (deltaxsquared + deltaysquared + deltazsquared) ** 0.5
+
+def make_random_points(size, resolution, number=-1):
+    '''
+    FOR TESTING, make some random points
+    can be used to find optimal values
+    '''
+    points = []
+    # Make random points
+    for i in range(0,size[0],resolution):
+        for j in range(0,size[1],resolution):
+            points.append((i,j,0))
+
+    # Remove uneven points
+    if len(points) % 2 != 0:
+        points = points[:-1]
+
+    # shuffle, cut and divide in start en end
+    random.shuffle(points)
+    points = points[:number]
+    ends = points[int(len(points)/2):]
+    starts = points[:int(len(points)/2)]
+
+    return starts, ends
+
+def length_score(data_file, wires, percentile, not_connected,
+                 cal_time, net_number, points_to_connect):
+    '''
+    Nice start of how we can "SCORE" our result
+    (Store in CSV????)
+    '''
+    # Retrive data
+    len_list = [len(i.route) for i in wires]
+    minlen_list = [abs(i.start[0] - i.end[0]) +
+                   abs(i.start[1] - i.end[1])
+                   for i in wires]
+
+    # Stats
+    data = {'netlist':net_number}
+    data['total_connected'] = percentile
+    data['true_len'] = sum(len_list)
+    data['longest'] = max(len_list)
+    data['shortest'] = min(len_list)
+    data['mean'] = data['true_len'] / len(len_list)
+    data['q25'] = len_list[int(len(len_list)/4)]
+    data['q75'] = len_list[-int(len(len_list)/4)]
+    data['cal_time'] = cal_time
+    data['len_percentile'] = sum(len_list)/sum(minlen_list)
+    data['starting order'] = str(points_to_connect)
+
+    print(f'\nThe score was: {percentile}')
+    output(data_file, data)
+    print(f'Data is saved in {data_file}')
+    return percentile
+
+def make_imported_points(points, netlist):
+    start_index, end_index = zip(*netlist)
+    starts = [(points[i][0],points[i][1],0) for i in start_index]
+    ends = [(points[i][0],points[i][1],0) for i in end_index]
+    return ends, starts
+
+def output(filename, data):
+    df = pd.read_excel(filename, sheet_name=0)
+    data['time'] = str(time.localtime())
+    df_new = pd.DataFrame(data, index = [1])
+    df_file = df.append(df_new, ignore_index=True, sort=False)
+    df_file.to_excel(filename)
---
+++ chipSolver/viz_test.py
@@ -0,0 +1,83 @@
+"""
+============
+3D animation
+============
+
+A simple example of an animated plot... In 3D!
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+
+
+def plot_anam(rawdata, size, multi = False):
+
+    if multi == False:
+        data = []
+        line = [[],[],[]]
+        max_len = 0
+        ends = []
+        for l in [i.route for i in rawdata]:
+            linex,liney, linez = zip(*l)
+            line[0] += linex
+            line[1] += liney
+            line[2] += linez
+            max_len += len(linex)
+            ends.append(len(line[0])-1)
+        data= np.array([line])
+
+    else:
+        data = []
+        max_len = 0
+        for l in [i.route for i in rawdata]:
+            line = [[],[],[]]
+            linex,liney, linez = zip(*l)
+            line[0] += linex
+            line[1] += liney
+            line[2] += linez
+
+            if len(linex) > max_len:
+                max_len = len(linex)
+
+            data.append(np.array(line))
+
+    def init():
+        #init lines
+        for line in lines:
+            line.set_data([], [])
+        return lines
+
+    def update_lines(num, dataLines, lines):
+        for line, data in zip(lines, dataLines):
+            # NOTE: there is no .set_data() for 3 dim data...
+            line.set_data(data[0:2, :num])
+            line.set_3d_properties(data[2, :num])
+            line.set_color('green')
+        return lines
+
+    # Attaching 3D axis to the figure
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    # Creating fifty line objects.
+    # NOTE: Can't pass empty arrays into 3d version of plot()
+    lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
+
+    # Setting the axes properties
+    ax.set_xlim3d([0.0, float(size[0])])
+    ax.set_xlabel('X')
+
+    ax.set_ylim3d([0.0, float(size[1])])
+    ax.set_ylabel('Y')
+
+    ax.set_zlim3d([0.0, float(size[2])])
+    ax.set_zlabel('Z')
+
+    ax.set_title('3D Test')
+
+    # Creating the Animation object
+    line_ani = animation.FuncAnimation(fig, update_lines, init_func=init, frames = max_len, fargs=(data, lines),
+                                           interval=200, blit=False)
+
+    plt.show()

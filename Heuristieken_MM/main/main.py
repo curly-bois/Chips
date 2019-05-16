8ef9fe0754811bdc20a388c6114f1305d19c1f25
Vipea <fringsmax@gmail.com>
2019-05-16 13:51:49 +0200

    solved plotting

--- Heuristieken_MM/main/main.py
+++ Heuristieken_MM/main/main.py
@@ -17,6 +17,7 @@ gridpoints = make_grid(grid)
 while counter < 1:
     # initializing the grid and making the points
     counter += 1
+
     matrix = make_matrix(gridpoints)
     to_be_connected = make_conlist(connections, matrix)
 
@@ -24,15 +25,12 @@ while counter < 1:
     new_connections = []
     print(f"THIS MANY IS LEFT: {int(len(unconnected_sets) / len(all_sets) * 100)}%")
 
-    # Breaks 15 % of connected sets and combines them with the unconnected sets to run the A algorithm on again
-
-    ## Hier heb je toch al een lijst van --> Unconnected??
-    for set in all_sets:
-        if not set.is_it_connected():
-            new_connections.append(set)
-
-solved_sets = hillsolve(50, matrix, all_sets, unconnected_sets, connected_sets)
-
-
-make_plot(solved_sets)
-make_xlsx(solved_sets,matrix,netlistname)
+if len(unconnected_sets) == 0:
+    print("eerste try")
+    make_plot(connected_sets)
+    make_xlsx(connected_sets,matrix,netlistname)
+else:
+    print("niet eerste try")
+    solved_sets = hillsolve(50, matrix, all_sets, unconnected_sets, connected_sets)
+    make_plot(solved_sets)
+    make_xlsx(solved_sets,matrix,netlistname)

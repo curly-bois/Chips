4bbca6f5ec30d2c2a479bd2f0bf76e36eae8f9d0
Floris Fok <43857896+FlorisFok@users.noreply.github.com>
2019-05-24 19:31:03 +0200

    Create README.mk

---
+++ chipSolver/README.mk
@@ -0,0 +1,14 @@
+# How to use
+
+* Change option file to desired settings, every row is a single set of setting.
+* Run main as followed:
+```
+python main.py [netlist number] [Option number/row number] [Repetitions] [methode]
+```
+methods: 'plant', 'sima', 'hill', 'local' and 'plantsima'.
+netlist number: 1,2,3,4,5 & 6
+options: depends on file.
+
+* Wait for a bit...
+* If plot is set True in the option file, you will get two plots
+* Other images are saved in generated figs file and the results are saved in the data file
